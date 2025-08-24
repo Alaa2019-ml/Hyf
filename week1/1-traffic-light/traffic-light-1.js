765ee8cb6312a9ec79454ef34acd9ba642c81359
Alaa <angnm38@gmail.com>
2025-08-24 10:01:41 +0200

    Move traffic-light.js file to weeek1 dir

---
+++ week1/1-traffic-light/traffic-light-1.js
@@ -0,0 +1,64 @@
+"use strict";
+/**
+ * The `state` property says what the traffic light's state (i.e. colour) is at
+ * that moment.
+ */
+const trafficLight = {
+  state: "green",
+};
+
+let rotations = 0;
+ do {
+   const currentState = trafficLight.state
+    console.log("The traffic light is on", currentState)
+
+     switch (currentState){
+        case "green":
+             trafficLight.state = "orange"
+             break
+         case "orange":
+             trafficLight.state = "red"
+             break
+         case "red":
+             trafficLight.state = "green"
+             rotations += 1
+             break
+     }
+} while (rotations < 2);
+/*
+while (rotations < 2) {
+  const currentState = trafficLight.state;
+  console.log("The traffic light is on", currentState);
+
+  switch (currentState){
+        case "green":
+             trafficLight.state = "orange"
+             break
+         case "orange":
+             trafficLight.state = "red"
+             break
+         case "red":
+             trafficLight.state = "green"
+             rotations += 1
+             break
+
+     }
+  // TODO
+  // if the color is green, turn it orange
+  // if the color is orange, turn it red
+  // if the color is red, add 1 to rotations and turn it green
+}
+
+*/
+
+/**
+ * The output should be:
+
+The traffic light is on green
+The traffic light is on orange
+The traffic light is on red
+The traffic light is on green
+The traffic light is on orange
+The traffic light is on red
+
+*/
